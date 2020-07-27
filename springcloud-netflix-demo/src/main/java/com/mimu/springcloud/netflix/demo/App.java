a4fd1dce634cf7a8534c06c9b2fcc8c3743c0efb
mimu <mimu@mimu-mbp.local>
2020-07-27 19:27:35 +0800

    project init

---
+++ springcloud-netflix-demo/src/main/java/com/mimu/springcloud/netflix/demo/App.java
@@ -0,0 +1,13 @@
+package com.mimu.springcloud.netflix.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
