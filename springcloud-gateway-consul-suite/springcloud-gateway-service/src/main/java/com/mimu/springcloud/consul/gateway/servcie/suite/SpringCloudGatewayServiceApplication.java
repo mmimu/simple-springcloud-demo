d75121a4397b2ad2f0ee63ac2d410bb83316e878
mimu <mimu@mimu-mbp.local>
2020-07-30 18:08:15 +0800

    update project

---
+++ springcloud-gateway-consul-suite/springcloud-gateway-service/src/main/java/com/mimu/springcloud/consul/gateway/servcie/suite/SpringCloudGatewayServiceApplication.java
@@ -0,0 +1,14 @@
+package com.mimu.springcloud.consul.gateway.servcie.suite;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringCloudGatewayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayServiceApplication.class, args);
+    }
+}
