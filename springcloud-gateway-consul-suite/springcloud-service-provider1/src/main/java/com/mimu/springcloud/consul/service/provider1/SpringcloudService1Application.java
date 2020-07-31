0dbfb4fdf6203f04fa893638fcd2dedac97f762e
mimu <mimu@mimu-mbp.local>
2020-07-31 19:21:26 +0800

    update project

---
+++ springcloud-gateway-consul-suite/springcloud-service-provider1/src/main/java/com/mimu/springcloud/consul/service/provider1/SpringcloudService1Application.java
@@ -0,0 +1,12 @@
+package com.mimu.springcloud.consul.service.provider1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudService1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudService1Application.class, args);
+    }
+}
