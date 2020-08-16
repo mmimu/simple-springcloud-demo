ccf9a035ccdc0e4a486d104025a0688e17bcfb91
mimu <mimu@mimu-mbp.local>
2020-08-16 10:27:23 +0800

    update project

--- springcloud-microservice-consumer/src/main/java/com/mimu/springcloud/ms/consumer/service/ISchoolInfoService.java
+++ springcloud-microservice-consumer/src/main/java/com/mimu/springcloud/ms/consumer/service/ISchoolInfoService.java
@@ -5,13 +5,14 @@ import com.mimu.springcloud.ms.consumer.request.SchoolRequest;
 import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "micro-service-school", fallbackFactory = SchoolInfoServiceFallbackFactory.class)
 public interface ISchoolInfoService {
 
-    @GetMapping("/school.do")
-    SchoolSchoolInfo getSchoolInfo1(@RequestParam("request") SchoolRequest request);
+    @GetMapping(value = "/school.do")
+    SchoolSchoolInfo getSchoolInfo1(@RequestBody SchoolRequest request);
 
     @GetMapping(value = "/school.do")
     SchoolSchoolInfo getSchoolInfo2(SchoolRequest request);
