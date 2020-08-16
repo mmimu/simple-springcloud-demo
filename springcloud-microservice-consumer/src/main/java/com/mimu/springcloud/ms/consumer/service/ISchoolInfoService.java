7c9d922177ed4c021190f4677e9dd8bcd22f2e0e
mimu <mimu@mimu-mbp.local>
2020-08-16 21:46:59 +0800

    update project

--- springcloud-microservice-consumer/src/main/java/com/mimu/springcloud/ms/consumer/service/ISchoolInfoService.java
+++ springcloud-microservice-consumer/src/main/java/com/mimu/springcloud/ms/consumer/service/ISchoolInfoService.java
@@ -2,13 +2,10 @@ package com.mimu.springcloud.ms.consumer.service;
 
 import com.mimu.springcloud.ms.consumer.model.SchoolSchoolInfo;
 import com.mimu.springcloud.ms.consumer.request.SchoolRequest;
-import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.lang.model.element.NestingKind;
 import java.util.Map;
 
 @FeignClient(value = "micro-service-school", fallbackFactory = SchoolInfoServiceFallbackFactory.class)
