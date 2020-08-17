80e7d4717c97b63dddb2f0cafde2d401206ee769
mimu <mimu@mimu-mbp.local>
2020-08-17 14:47:01 +0800

    update project

--- springcloud-gateway-consul-suite/springcloud-service-consumer/src/main/java/com/mimu/springcloud/consul/service/consumer/config/ProjectApplicationConfig.java
+++ springcloud-gateway-consul-suite/springcloud-service-consumer/src/main/java/com/mimu/springcloud/consul/service/consumer/config/ProjectApplicationConfig.java
@@ -3,6 +3,9 @@ package com.mimu.springcloud.consul.service.consumer.config;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import feign.Feign;
+import feign.Retryer;
+import feign.querymap.BeanQueryMapEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,5 +18,16 @@ public class ProjectApplicationConfig {
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return objectMapper;
+    }
+
+    /**
+     * 改 builder 主要指定 feign 的 参数 解析
+     * 使用 @SpringQueryMap 中 的对象参数 可以解析通过继承的 父类中的 参数字段
+     *
+     * @return
+     */
+    @Bean
+    public Feign.Builder feignBuilder() {
+        return Feign.builder().queryMapEncoder(new BeanQueryMapEncoder()).retryer(Retryer.NEVER_RETRY);
     }
 }
